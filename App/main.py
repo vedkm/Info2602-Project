4f9a0de6af00ccc81ca1a05c79d0b80b3fd52e18
Ved Mahadeo <vedkailash1@gmail.com>
2022-04-08 13:14:05 -0400

    init

--- App/main.py
+++ App/main.py
@@ -1,40 +1,62 @@
-import json
-from flask import Flask, request, render_template
-from flask_jwt import JWT, jwt_required, current_identity
-from sqlalchemy.exc import IntegrityError
-from datetime import timedelta 
+import os
+from flask import Flask
+from flask_login import LoginManager, current_user
+from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+from datetime import timedelta
 
-from models import db
 
-''' Begin boilerplate code '''
-def create_app():
-  app = Flask(__name__, static_url_path='')
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-  app.config['SECRET_KEY'] = "MYSECRET"
-  app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7) 
-  db.init_app(app)
-  return app
+from App.database import create_db, get_migrate
+
+from App.controllers import (
+    setup_jwt
+)
+
+from App.views import (
+    user_views,
+    api_views
+)
+
+views = [
+    user_views,
+    api_views
+]
+
+def add_views(app, views):
+    for view in views:
+        app.register_blueprint(view)
+
+
+def loadConfig(app, config):
+    app.config['ENV'] = os.environ.get('ENV', 'DEVELOPMENT')
+    if app.config['ENV'] == "DEVELOPMENT":
+        app.config.from_object('App.config')
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+        app.config['JWT_EXPIRATION_DELTA'] =  timedelta(days=int(os.environ.get('JWT_EXPIRATION_DELTA')))
+        app.config['DEBUG'] = os.environ.get('ENV').upper() != 'PRODUCTION'
+        app.config['ENV'] = os.environ.get('ENV')
+    for key, value in config.items():
+        app.config[key] = config[key]
+
+def create_app(config={}):
+    app = Flask(__name__, static_url_path='/static')
+    CORS(app)
+    loadConfig(app, config)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
+    app.config['UPLOADED_PHOTOS_DEST'] = "App/uploads"
+    photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
+    configure_uploads(app, photos)
+    add_views(app, views)
+    create_db(app)
+    setup_jwt(app)
+    app.app_context().push()
+    return app
 
 app = create_app()
-
-app.app_context().push()
-
-''' End Boilerplate Code '''
-
-''' Set up JWT here '''
-
-''' End JWT Setup '''
-
-# edit to query 50 pokemon objects and send to template
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-@app.route('/app')
-def client_app():
-  return app.send_static_file('app.html')
-
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+migrate = get_migrate(app)
